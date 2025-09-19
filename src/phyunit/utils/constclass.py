b37d12c7529d98734d57027a70aed7f92603ef71
Dait <dyj24@mails.tsinghua.edu.cn>
2025-09-19 17:13:12 +0800

    modify

--- src/phyunit/utils/constclass.py
+++ src/phyunit/utils/constclass.py
@@ -1,29 +1,39 @@
 '''
 ConstClass is a class storing static constants.
-It's similar to Enum, but members of Enum need an extra value attribute
-to access its value, and I think it's unnecessary. 
-So here is ConstClass.
+It's similar to Enum, but members of Enum need an extra value attribute `value`
+to access its value, and I think it's unnecessary, so I use ConstClass.
 '''
 
+from typing import Any, NoReturn
 
 __all__ = ['ConstMeta', 'ConstClass']
 
 
 class ConstMeta(type):
-    def __new__(metacls, cls, bases, attrs):  # type: ignore
-        return super().__new__(metacls, cls, bases, attrs)
+    def __new__(metacls, cls, bases, attrs, /, **kwds: Any):
+        return super().__new__(metacls, cls, bases, attrs, **kwds)
 
-    def __init__(self, cls, bases, attrs):
-        return super().__init__(cls, bases, attrs)
-    
-    def __call__(self, *args, **kwds):
-        raise TypeError('Cannot instantiate objects.')
+    def __init__(metacls, cls, bases, attrs, /, **kwds: Any) -> None:
+        return super().__init__(cls, bases, attrs, **kwds)
 
-    def __setattr__(self, name, value):
-        raise AttributeError("Cannot modify constant.")
+    def __call__(metacls, *args: Any, **kwds: Any) -> NoReturn:
+        '''
+        No, this class restores constants, you can NOT instantiate objects.
+        please access constants via class attributes.
+        '''
+        raise TypeError(metacls.__call__.__doc__)
 
-    def __delattr__(self, name):
-        raise AttributeError("Cannot delete constant.")
+    def __setattr__(cls, name, value) -> NoReturn:
+        '''
+        No, this class restores constants, you can NOT modify them.
+        '''
+        raise AttributeError(cls.__class__.__setattr__.__doc__)
+
+    def __delattr__(cls, name) -> NoReturn:
+        '''
+        No, this class restores constants, you can NOT delete them.
+        '''
+        raise AttributeError(cls.__class__.__delattr__.__doc__)
 
 
 class ConstClass(metaclass=ConstMeta):
