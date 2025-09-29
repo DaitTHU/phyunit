eea608e8132c7dbf34db6a1cd7df16a211cd370e
Dait <dyj24@mails.tsinghua.edu.cn>
2025-09-29 15:15:51 +0800

    edit doc

--- src/phyunit/dimensionconst.py
+++ src/phyunit/dimensionconst.py
@@ -1,28 +1,32 @@
+from .dimension import DIMENSIONLESS, Dimension
 from .utils.constclass import ConstClass
 
-from .dimension import Dimension, DIMENSIONLESS
 
 class DimensionConst(ConstClass):
     '''
-    `DimensionConst` is a class containing constant `Dimension` objects
-    objects, like dimensionless, 7 SI base units, and other derived units.
-    Units with different physical meanings sharing the same dimension,
-    like energy and work, have to share the same name `ENERGY`.
-    This constraint exists because dimensions are mathematical abstractions
-    that represent physical quantities, and multiple quantities with the same
-    dimensional formula (e.g., energy and work) are treated as equivalent
-    in dimensional analysis. This ensures consistency in calculations and
-    avoids ambiguity when performing operations involving these quantities.
+    use this class to access commonly used `Dimension` constants.
 
-    Example usage:
-        >>> print(DimensionConst.VELOCITY)
-        T⁻¹L
+    >>> print(DimensionConst.VELOCITY)
+    T⁻¹L
 
-        >>> print(DimensionConst.FORCE)
-        T⁻²LM
+    >>> print(DimensionConst.FORCE)
+    T⁻²LM
+
+    NOTE
+    different physical quantities may have the same dimension,
+    like _energy_ and _work_.
+
+    Be careful when using these dimensions,
+    especially in `set` or as keys in `dict`:
+
+    >>> {DimensionConst.ENERGY: 1, DimensionConst.WORK: 2}  # same key!
+    {Dimension(T=-2, L=2, M=1): 2}
     '''
-    DIMENSIONLESS = DIMENSIONLESS  # default singleton
+
+    DIMENSIONLESS = DIMENSIONLESS
+
     # 7 eigen SI base units
+
     TIME = Dimension(T=1)
     LENGTH = Dimension(L=1)
     MASS = Dimension(M=1)
@@ -30,7 +34,9 @@ class DimensionConst(ConstClass):
     THERMODYNAMIC_TEMPERATURE = Dimension(Theta=1)
     AMOUNT_OF_SUBSTANCE = Dimension(N=1)
     LUMINOUS_INTENSITY = Dimension(J=1)
+
     # straight derived
+
     ANGLE = PHASE_ANGLE = PLANE_ANGLE = SOLID_ANGLE = DIMENSIONLESS
     WAVENUMBER = 1 / LENGTH
     AREA = LENGTH**2
@@ -38,7 +44,9 @@ class DimensionConst(ConstClass):
     FREQUENCY = 1 / TIME
     ACTIVITY = FREQUENCY  # of a radionuclide
     TEMPERATURE = THERMODYNAMIC_TEMPERATURE
+
     # kinematics and dynamic
+
     VELOCITY = LENGTH / TIME
     ACCELERATION = VELOCITY / TIME
     FORCE = MASS * ACCELERATION
@@ -52,7 +60,9 @@ class DimensionConst(ConstClass):
     MOMENTUM = MASS * VELOCITY
     DYNAMIC_VISCOSITY = PRESSURE * TIME
     KINEMATIC_VISCOSITY = AREA / TIME
+
     # electrodynamics
+
     ELECTRIC_CHARGE = ELECTRIC_CURRENT * TIME
     VOLTAGE = POWER / ELECTRIC_CURRENT
     ELECTRIC_POTENTIAL = ELECTROMOTIVE_FORCE = VOLTAGE
@@ -65,13 +75,21 @@ class DimensionConst(ConstClass):
     MAGNETIC_INDUCTION = MAGNETIC_FLUX_DENSITY
     MAGNETIC_FIELD_STRENGTH = ELECTRIC_CURRENT / LENGTH
     INDUCTANCE = MAGNETIC_FLUX / ELECTRIC_CURRENT
+
     # luminous
+
     LUMINOUS_FLUX = LUMINOUS_INTENSITY * SOLID_ANGLE
     ILLUMINANCE = LUMINOUS_FLUX / AREA
     LUMINANCE = LUMINOUS_INTENSITY / AREA
+
     # nuclear radiation
+
     KERMA = ENERGY / MASS
     ABSORBED_DOSE = EQUIVALENT_DOSE = KERMA  # of ionising radiation
     EXPOSURE = ELECTRIC_CHARGE / MASS  # X-ray and γ-ray
+
     # chemistry
+
     CATALYTIC_ACTIVITY = AMOUNT_OF_SUBSTANCE / TIME
+
+    
