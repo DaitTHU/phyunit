86d987c3b037f017aa7f3d804c014dbcf66b94ed
Dait <dyj24@mails.tsinghua.edu.cn>
2025-10-09 01:28:34 +0800

    add au, nu support

--- src/phyunit/SI/__init__.py
+++ src/phyunit/SI/__init__.py
@@ -1,5 +1,6 @@
-from .constants import SI
+from . import conversion
+from .constants import SI, particle
 from .prefixes import prefix
 from .units import si
 
-__all__ = ['prefix', 'si', 'SI']
+__all__ = ['prefix', 'si', 'SI', 'particle', 'conversion']
