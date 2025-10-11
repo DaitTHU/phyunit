3e6bba04ae20955aed3438fd3009522717c9b6ee
Dait <dyj24@mails.tsinghua.edu.cn>
2025-10-12 00:47:40 +0800

    rm alias

--- src/phyunit/_data/prefixes.py
+++ src/phyunit/_data/prefixes.py
@@ -7,21 +7,18 @@ class PrefixData:
     Attributes:
         factor (str): e.g., 1000 for kilo-
         name (str | list[str]): e.g., 'kilo', 'mega'
-        alias (None | str | list[str]): alternative symbols
     '''
 
-    __slots__ = ('factor', 'name', 'alias')
+    __slots__ = ('factor', 'name')
 
-    def __init__(self, factor: float, name: str | list[str], *,
-                 alias: None | str | list[str] = None) -> None:
+    def __init__(self, factor: float, name: str | list[str]) -> None:
         self.factor = factor
         self.name = [name] if isinstance(name, str) else name
-        self.alias = [alias] if isinstance(alias, str) else alias
 
     def __hash__(self) -> int: return hash((self.factor, self.name[0]))
 
 
-PREFIX: dict[str, PrefixData] = {
+__PREFIX_LIB: dict[str | tuple[str, ...], PrefixData] = {
     # whole unit
     'Q': PrefixData(1e30, 'quetta'),
     'R': PrefixData(1e27, 'ronna'),
@@ -32,7 +29,7 @@ PREFIX: dict[str, PrefixData] = {
     'T': PrefixData(1e12, 'tera'),
     'G': PrefixData(1e9, 'giga'),
     'M': PrefixData(1e6, 'mega'),
-    'k': PrefixData(1e3, 'kilo', alias='K'),
+    ('k', 'K'): PrefixData(1e3, 'kilo'),
     'h': PrefixData(1e2, 'hecto'),
     'da': PrefixData(1e1, 'deka'),
     '': PrefixData(1, ''),
@@ -40,7 +37,7 @@ PREFIX: dict[str, PrefixData] = {
     'd': PrefixData(1e-1, 'deci'),
     'c': PrefixData(1e-2, 'centi'),
     'm': PrefixData(1e-3, 'milli'),
-    'µ': PrefixData(1e-6, 'micro', alias=['u', 'μ']),  # chr(0xB5): chr(0x3BC)
+    ('µ', 'μ', 'u'): PrefixData(1e-6, 'micro'),  # chr(0xB5), chr(0x3BC)
     'n': PrefixData(1e-9, 'nano'),
     'p': PrefixData(1e-12, 'pico'),
     'f': PrefixData(1e-15, 'femto'),
@@ -50,6 +47,11 @@ PREFIX: dict[str, PrefixData] = {
     'r': PrefixData(1e-27, 'ronto'),
     'q': PrefixData(1e-30, 'quecto'),
 }
+
+PREFIX = {
+    prefix[0] if isinstance(prefix, tuple) else prefix: data 
+    for prefix, data in __PREFIX_LIB.items()
+}
 '''prefix {symbol: data}'''
 
 PREFIX_NAME: dict[str, str] = {
@@ -58,7 +60,7 @@ PREFIX_NAME: dict[str, str] = {
 '''prefix {name: symbol}'''
 
 PREFIX_ALIAS: dict[str, str] = {
-    alias: prefix for prefix, data in PREFIX.items() if data.alias is not None
-    for alias in data.alias
+    alias: prefix[0] for prefix in __PREFIX_LIB if isinstance(prefix, tuple)
+    for alias in prefix[1:]
 }
 '''prefix {alias symbol: symbol}'''
